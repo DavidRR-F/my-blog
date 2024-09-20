266c021b9f1eec71a800327f83134c4a3b96b945
DavidRR-F <david.rosefranklin96@gmail.com>
2024-09-19 20:00:13 -0400

    init

--- utils/convert.py
+++ utils/convert.py
@@ -2,12 +2,12 @@ from obsidian_to_hugo import ObsidianToHugo
 import os
 
 
-def main()
-    pd = os.path.dirname(os.getcwd())
+def main():
+    cwd = os.getcwd()
 
     obsidian_to_hugo = ObsidianToHugo(
-        obsidian_vault_dir=os.path.join(pd, 'obsidian'),
-        hugo_content_dir=os.path.join(pd, 'blog', 'content')
+        obsidian_vault_dir=os.path.join(cwd, 'obsidian'),
+        hugo_content_dir=os.path.join(cwd, 'blog', 'content')
     )
 
     obsidian_to_hugo.run()
